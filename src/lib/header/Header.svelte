d97011bfa3f68498c06428f510b34c5517854f3a
Donghoon Shin <ssshyhy@snu.ac.kr>
2022-05-31 14:54:39 +0900

    Update Header.svelte

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -10,10 +10,10 @@
 		<div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
 		  <div class="flex-shrink-0 flex items-center">
 			<a href="/">
-				<img class="block lg:hidden h-8 w-auto" src="genome.png" alt="Workflow">
+				<img class="block lg:hidden h-8 w-auto" src="/genome.png" alt="Workflow">
 			</a>
 			<a href="/">
-				<img class="hidden lg:block h-8 w-auto" src="genome_expanded.png" alt="Workflow">
+				<img class="hidden lg:block h-8 w-auto" src="/genome_expanded.png" alt="Workflow">
 			</a>
 		  </div>
 		</div>
@@ -24,7 +24,7 @@
 			<div>
 			  <button type="button" class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
 				<span class="sr-only">Open user menu</span>
-				<img class="h-8 w-8 rounded-full" src="profile.jpg" alt="">
+				<img class="h-8 w-8 rounded-full" src="/profile.jpg" alt="">
 			  </button>
 			</div>
   
